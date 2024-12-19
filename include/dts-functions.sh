39e7b802856a2db789f9b84ad4759abebe57566a
Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>
2024-12-19 11:27:05 +0100

    dts-functions.sh, dts-subscription.sh: fix empty credentials input
    Add check for empty dpp credentials input from the user.
    If at any moment entered credentials are empty, print warning and abort the process.
    
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>
    Co-authored-by: Michał Iwanicki <michal.iwanicki@3mdeb.com>

--- include/dts-functions.sh
+++ include/dts-functions.sh
@@ -1449,9 +1449,12 @@ main_menu_options(){
       return 0
       ;;
     "${DPP_KEYS_OPT}")
-      local _result
+      # Return if there was an issue when asking for credentials:
+      if ! get_dpp_creds; then
+        read -p "Press Enter to continue."
+        return 0
+      fi
 
-      get_dpp_creds
 
       # Check for Dasharo Firmware for the current platform, continue to
       # packages after checking:
