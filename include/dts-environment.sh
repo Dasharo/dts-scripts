fb01744bc5d7590d27af0929d51bfbaa52734e38
Pawel Langowski <pawel.langowski@3mdeb.com>
2025-03-21 11:20:26 +0100

    Add public MinIO credentials for HCL reports
    Signed-off-by: Pawel Langowski <pawel.langowski@3mdeb.com>

--- include/dts-environment.sh
+++ include/dts-environment.sh
@@ -28,8 +28,8 @@ DPP_CREDENTIAL_FILE="/etc/cloud-pass"
 FW_STORE_URL="${FW_STORE_URL_DEV:-https://dl.3mdeb.com/open-source-firmware/Dasharo}"
 FW_STORE_URL_DPP="dl.dasharo.com"
 CLOUD_REQUEST="X-Requested-With: XMLHttpRequest"
-BASE_CLOUDSEND_LOGS_URL="39d4biH4SkXD8Zm"
-BASE_CLOUDSEND_PASSWORD="1{\[\k6G"
+BASE_HCL_USERNAME="public-hcl"
+BASE_HCL_PASSWORD="Vintage-Yearly-Vaporizer-Cubicle"
 DEPLOY_REPORT="false"
 
 # DTS menu options:
